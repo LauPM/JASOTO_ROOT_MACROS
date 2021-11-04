5c24010e3d8bf472b306f907d337642d0ef893fc
rodralva <rodrigo.alvarez@ciemat.es>
2021-11-04 10:40:27 +0100

    typo

--- maptest.C
+++ maptest.C
@@ -7,7 +7,7 @@ void maptest(){
     const Run_map map_of_runs=Run_map("laura_la_lia.json");
 
     for (auto i:map_of_runs.json_map) {
-        if (i["threshold"]>50 && i["threshold"]!="" && ){cout<<i["run"]<<"  "<< i["threshold"]<<endl;}
+        if (i["threshold"]>50 && i["threshold"]!="" ){cout<<i["run"]<<"  "<< i["threshold"]<<endl;}
     }
 
 }
